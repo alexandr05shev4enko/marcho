8d121216b66a3877a0847cb3ea9dfd324006a3f7
alexandr05shev4enko <alex050798@gmail.com>
2020-11-21 14:54:48 +0200

    added top-slider section

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,7 @@ function browsersync() {
   browserSync.init({
     server: {
       baseDir: 'app/'
-    },
-    browser: ['chrome']
+    }
   })
 }
 
@@ -34,6 +33,7 @@ function styles(){
 function scripts(){
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
